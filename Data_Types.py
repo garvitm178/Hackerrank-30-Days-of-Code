a04edd227d7144308794d949e165cf024c4157b9
Garvit Mehta <35095197+garvitm178@users.noreply.github.com>
2021-06-19 02:58:51 +0530

    add new file

---
+++ Data_Types.py
@@ -0,0 +1,14 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+num1 = int(input())
+num2 = float(input())
+str1 = input()
+# Print the sum of both integer variables on a new line.
+print(i+num1)
+# Print the sum of the double variables on a new line.
+print(d+num2)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+str1)
